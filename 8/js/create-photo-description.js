df4c1f632c775d28d69cdf7c9dee31d0546a4c3b
Keks <keks@htmlacademy.ru>
2024-11-25 17:32:25 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/create-photo-description.js
@@ -0,0 +1,69 @@
+import {getRandomInteger} from './utils';
+import {getRandomArrayElement} from './utils';
+import {createIdGenerator} from './utils';
+
+const MIN_NUMBER_AVATAR = 1;
+const MAX_NUMBER_AVATAR = 6;
+const MIN_LIKES = 15;
+const MAX_LIKES = 250;
+const QUANTITY_ARRAYS = 25;
+const MIN_COMMENTS = 0;
+const MAX_COMMENTS = 30;
+
+const NAMES = [
+  'Иван',
+  'Хуан Себастьян',
+  'Мария',
+  'Кристоф',
+  'Виктор',
+  'Юлия',
+  'Люпита',
+  'Вашингтон'
+];
+
+const DESCRIPTION = [
+  'Фото гор',
+  'Фото животного',
+  'Фото леса',
+  'Фото дома'
+];
+
+const MESSAGE = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
+
+const generateCommentsId = createIdGenerator();
+const generatePhotoId = createIdGenerator();
+const generateUrlId = createIdGenerator();
+
+const createComments = () => {
+  const randomAvatar = getRandomInteger(MIN_NUMBER_AVATAR, MAX_NUMBER_AVATAR);
+
+  return {
+    id: (generateCommentsId()),
+    avatar: `img/avatar-${ randomAvatar }.svg`,
+    message: getRandomArrayElement(MESSAGE),
+    name: getRandomArrayElement(NAMES),
+  };
+};
+
+const createDescriptionPhoto = () =>{
+  const randomLikes = getRandomInteger(MIN_LIKES, MAX_LIKES);
+  return {
+    id: (generatePhotoId()),
+    url: `photos/${ generateUrlId() }.jpg`,
+    description: getRandomArrayElement(DESCRIPTION),
+    likes: randomLikes,
+    comments: Array.from({length: getRandomInteger(MIN_COMMENTS, MAX_COMMENTS)}, createComments)
+  };
+};
+
+const similarDescriptionPhoto = Array.from ({length: QUANTITY_ARRAYS}, createDescriptionPhoto);
+
+export {similarDescriptionPhoto};
---
+++ 8/js/main.js
@@ -0,0 +1,3 @@
+import {similarDescriptionPhoto} from './insert-thumbnails';
+
+similarDescriptionPhoto();
