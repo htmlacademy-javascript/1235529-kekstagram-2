f9293eb44d59152e9dd03e8e65aa6afee59e6fcb
Кирпиченков Никита <kirpich.nikita98@mail.ru>
2024-12-19 17:46:57 +0300

    Добавил функцию очистки миниатюр, и открытия фильтров

--- js/insert-thumbnails.js
+++ js/insert-thumbnails.js
@@ -1,11 +1,19 @@
 import {createThumbnail} from './rendering-pictures.js';
 import {getData} from './api.js';
 import {showPopup} from './popup-error.js';
+import {openFilter} from './photo-filter.js';
 
 const picturesContainer = document.querySelector('.pictures');
 const fragment = document.createDocumentFragment();
 
+const clearThumbnails = () => {
+  const allPictures = picturesContainer.querySelectorAll('.picture');
+  allPictures.forEach((picture) => picture.remove());
+};
+
+
 const insertThumbnails = (pictures) => {
+  clearThumbnails();
   pictures.forEach((photo) => {
     fragment.append(createThumbnail(photo));
     picturesContainer.append(fragment);
@@ -13,5 +21,11 @@ const insertThumbnails = (pictures) => {
 };
 
 getData()
-  .then((pictures)=> insertThumbnails(pictures))
+  .then((pictures)=> {
+    insertThumbnails(pictures);
+    openFilter(pictures);
+  })
   .catch((err) => showPopup(err.message));
+
+
+export {insertThumbnails};
